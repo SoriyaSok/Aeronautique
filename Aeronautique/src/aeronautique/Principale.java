e0bde02698490df287baa403d385b7704cade0d2
ib <ib@WINIB-TLVGJHMJ7.local>
2016-06-08 14:44:21 +0200

    ajout d'un avion3

--- Aeronautique/src/aeronautique/Principale.java
+++ Aeronautique/src/aeronautique/Principale.java
@@ -5,8 +5,10 @@ public class Principale {
 	public static void main(String[] args) {
 		Avion avion1 = new Avion(1, "Airbus300", 350, "Paris");
 		Avion avion2 = new Avion(2, "Airbus300", 350, "Paris");
+		Avion avion3 = new Avion(3, "Airbus300", 350, "Paris");
 		System.out.println(avion1);
 		System.out.println(avion2);
+		System.out.println(avion3);
 
 	}
 
